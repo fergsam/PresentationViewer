368b16fc42b898f3a6acd841fe6d50e277d56bf4
Sam Ferguson <sferguson@middlebury.edu>
2017-10-02 11:38:27 +1300

    Initial Commit

---
+++ PresentationViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PresentationViewer
+//
+//  Created by Sam Ferguson on 10/2/17.
+//  Copyright © 2017 Sam Ferguson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PresentationViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PresentationViewer
+//
+//  Created by Sam Ferguson on 10/2/17.
+//  Copyright © 2017 Sam Ferguson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
